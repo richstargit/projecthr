7b83450b749d8b5d169af8c6580be70d08a7996c
unknown <siwakchuhirun2004@gmail.com>
2025-07-29 15:33:16 +0700

    contact page

--- component/Cardpicture.tsx
+++ component/Cardpicture.tsx
@@ -1,11 +1,11 @@
 export default function Cardpicture() {
   return (
     <div className="flex flex-col lg:flex-row w-full border-2 shadow-lg rounded-xl lg:min-h-120 min-h-50">
-      <div className="md:basis-[40%] w-full">
-        <img src="/image/C_mainpage.png" alt="" className="object-cover h-full p-2" />
+      <div className="md:w-[40%] w-full">
+        <img src="/image/C_mainpage.png" alt="" className="object-cover p-2" />
       </div>
 
-      <div className="md:basis-[60%] w-full p-4">
+      <div className="md:w-[60%] w-full p-4">
         <h1 className="text-4xl font-bold">hello</h1>
         <p className="mt-5">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta corrupti magnam ad
